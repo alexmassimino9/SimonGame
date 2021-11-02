ec7165176c86b96c2401441fe7cf0a11673774ce
Alexander J Massimino <alexmassimino9@gmail.com>
2021-11-02 06:07:03 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
 
 const buttonColors = ["red", "blue", "green", "yellow"];
-
+const gamePattern = [];
 
 function nextSequence(){
   let randomNumber = Math.floor(Math.random()*4)
-  const randomChosenColor = buttonColors[randomNumber]
-  console.log(randomChosenColor);
+  const randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor)
+  console.log(gamePattern);
+let x =   $("#"+ randomChosenColor).attr("id");
+console.log(x)
+
 };
 
 nextSequence();

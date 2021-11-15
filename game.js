2213eb2cca1805dd497c6928fcf09c81e91c0025
Alexander J Massimino <alexmassimino9@gmail.com>
2021-11-14 17:41:58 -0800

    Added step 2 and step 3

--- game.js
+++ game.js
@@ -1,9 +1,23 @@
 var buttonColors = ["red", "blue", "green", "yellow"]
 var gamePattern = []
+var userClickedPattern []
+
 function nextSequence (){
   var randomNumber = Math.floor(Math.random()*4)
   var randomChosenColor = buttonColors[randomNumber]
   gamePattern.push(randomChosenColor)
   console.log(gamePattern)
+  $("#"+ randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
+  // let audio = new Audio("sounds/" + $("#"+randomChosenColor)+ ".mp3")
+
 }
+
+$(".btn").click(function(){
+
+  var userChosenColor = $(this).attr("id")
+  alert(userChosenColor)
+
+
+})
 nextSequence()
+// at step 4
